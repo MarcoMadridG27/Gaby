1c0773625713df68180111e3bb6933cb6067f2ae
Marco <marco.madrid@utec.edu.pe>
2025-08-03 13:22:46 -0500

    arreglos

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: true, // ⚠️ Temporal
+        ignoreDuringBuilds: true,
     },
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'gabymarco.s3.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'gabymarco.s3.us-east-1.amazonaws.com',
                 port: '',
                 pathname: '/**',
             },
